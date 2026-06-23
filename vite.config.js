ecdd5a7b72e94826dd32d16f6e3e4d97a14a9d9d
Riemar <riemsjr534@gmail.com>
2026-06-23 08:04:18 +0800

    A

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173,
     proxy: {
-      "/api": "http://localhost:5000"
+      "/api": {
+        target: "http://127.0.0.1:5070",
+        changeOrigin: true,
+        secure: false,
+      }
     }
   }
 });
